2c0887288838974c35f98b497e735b518277c100
Sage <106433812+sageherb@users.noreply.github.com>
2025-09-04 14:26:19 +0900

    Editor UI 레이아웃 완성 (#32)
    ## 개요
    
    Editor UI 레이아웃 완성
    
    ## 작업 내용
    
    - [x] 기존 에디터 태그 구조 변경
    - [x] CSS 적용
    
    ## 기타
    
    남은 몇 시간 힘내요

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -4,33 +4,39 @@ import apiDocs from "../../api/documents";
 export default async function Editor({ id }) {
   /* editor 기본 구조 생성 */
   const section = document.querySelector("#section");
-  // title과 contents를 각각의 div로 구성
-  const title = document.createElement("div");
-  const contents = document.createElement("div");
-  title.id = "title";
-  contents.id = "contents";
+  section.innerHTML = "";
 
-  section.appendChild(title);
-  section.appendChild(contents);
+  const div = document.createElement("div");
+  div.className = "document-detail";
+
+  // title과 contents를 각각의 div로 구성
+  const title = document.createElement("h1");
+  title.id = "title";
+  title.setAttribute("contenteditable", "true");
+  // title.setAttribute("data-placeholder", "새 페이지");
+
+  const contents = document.createElement("textarea");
+  contents.id = "contents";
+  // contents.setAttribute("contenteditable", "true");
+  // contents.setAttribute("data-placeholder", "내용을 입력하세요.");
+
+  div.appendChild(title);
+  div.appendChild(contents);
+  section.appendChild(div);
 
   /* api */
   const docData = await apiDocs.get(id);
 
-  /* 문서 내용 렌더링 */
-  // 텍스트 입력 가능
-  const titleText = document.createElement("textarea");
-  titleText.id = "titleText";
-  const contentsText = document.createElement("textarea");
-  contentsText.id = "contentsText";
+  // 텍스트 입력 가능, title/contents 내용이 없는 경우 포함
+  if (docData.title === null || docData.title === undefined) {
+    title.innerText = "새 페이지";
+  } else {
+    title.innerText = docData.title;
+  }
 
-  if (docData.title === null || docData.title === undefined) titleText.value = "새 페이지";
-  else titleText.value = docData.title;
-  if (docData.content === null || docData.content === undefined)
-    contentsText.value = "내용을 입력하세요.";
-  else contentsText.value = docData.content;
-
-  title.appendChild(titleText);
-  contents.appendChild(contentsText);
-
-  /* 문서 생성 */
+  if (docData.content === null || docData.content === undefined) {
+    contents.value = "내용을 입력하세요.";
+  } else {
+    contents.value = docData.content;
+  }
 }
--- src/pages/EmptyPage.js
+++ src/pages/EmptyPage.js
@@ -3,7 +3,7 @@ import svg from "/public/assets/notion.svg?url";
 export default function EmptyPage() {
   const section = document.querySelector("#section");
   section.innerHTML = `
-    <div class="note-detail">
-      <img class="note-detail-placeholder" src="${svg}" alt="Notion Icon">
+    <div class="document-detail">
+      <img class="document-detail-placeholder" src="${svg}" alt="Notion Icon">
     </div>`;
 }
